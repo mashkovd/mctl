2b673447ee39d5d63d9139a66a209f031e54c508
dmitrii.mashkov <mashkovd@live.ru>
2023-11-26 22:10:30 +0100

    update kube.tf fix coalesce

--- kube.tf
+++ kube.tf
@@ -35,13 +35,13 @@ module "kube-hetzner" {
 
   # * Your ssh public key
   ssh_public_key = coalesce(
-    var.ssh_public_key,
+    length(var.ssh_public_key) > 0 ? var.ssh_public_key : null,
     fileexists("~/.ssh/id_ed25519.pub") ? file("~/.ssh/id_ed25519.pub") : null
   )
   # * Your private key must be "ssh_private_key = null" when you want to use ssh-agent for a Yubikey-like device authentication or an SSH key-pair with a passphrase.
   # For more details on SSH see https://github.com/kube-hetzner/kube-hetzner/blob/master/docs/ssh.md
   ssh_private_key = coalesce(
-    var.ssh_private_key,
+    length(var.ssh_private_key) > 0 ? var.ssh_private_key : null,
     fileexists("~/.ssh/id_ed25519") ? file("~/.ssh/id_ed25519") : null
   )
   # You can add additional SSH public Keys to grant other team members root access to your cluster nodes.
