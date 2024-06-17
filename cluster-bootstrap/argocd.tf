19158d0ed939d3c6cc0b344b5bb2b65dbea8d6d9
Dmitrii.Mashkov <dmitrii.mashkov@libertexgroup.com>
2024-06-17 10:49:13 +0200

    upgrade argocd

--- cluster-bootstrap/argocd.tf
+++ cluster-bootstrap/argocd.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.49.0"
+  version          = "7.1.3"
   namespace        = "argocd"
   create_namespace = true
   wait             = true
